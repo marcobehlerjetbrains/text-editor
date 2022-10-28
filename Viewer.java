43189f6baaae3fe289808b765ee882a22b352cce
Marco Behler <marco@marcobehler.com>
2022-10-28 17:57:21 +0200

    testingfgdrfgdf

--- Viewer.java
+++ Viewer.java
@@ -237,6 +237,8 @@ public class Viewer {
         originalAttributes = LibC.Termios.of(termios);
         System.out.println("originalAttributes = " + originalAttributes);
 
+        System.out.println(termios.c_lflag  + " ------ " + (termios.c_lflag & ~LibC.ECHO));
+
         termios.c_lflag &= ~(LibC.ECHO | LibC.ICANON | LibC.IEXTEN | LibC.ISIG);
         termios.c_iflag &= ~(LibC.IXON | LibC.ICRNL);
         termios.c_oflag &= ~(LibC.OPOST);
