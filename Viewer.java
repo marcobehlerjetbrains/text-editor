09c35cdda5bbfe7063ea783ab21679bbc9024e45
Marco Behler <marco.behler@jetbrains.com>
2022-11-09 14:48:24 +0100

    proper os support

--- Viewer.java
+++ Viewer.java
@@ -1,15 +1,16 @@
-import com.sun.jna.LastErrorException;
-import com.sun.jna.Library;
-import com.sun.jna.Native;
-import com.sun.jna.Structure;
+import com.sun.jna.*;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.win32.StdCallLibrary;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Stream;
+
+import static com.sun.jna.Platform.isMac;
+import static com.sun.jna.Platform.isWindows;
 
 public class Viewer {
 
@@ -22,13 +23,15 @@ public class Viewer {
             PAGE_UP = 1006,
             PAGE_DOWN = 1007,
             DEL = 1008;
-    private static LibC.Termios originalAttributes;
+
     private static int rows = 10;
     private static int columns = 10;
 
     private static int cursorX = 0, cursorY = 0, offsetY = 0;
 
     private static List<String> content = List.of();
+
+    private static Terminal t = isMac() ? new MacOSTerminal()  : isWindows() ? new WindowsTerminal() : new UnixTerminal();
 
     public static void main(String[] args) throws IOException {
        // System.out.println("Hello World");
@@ -38,7 +41,7 @@ public class Viewer {
 
 
         openFile(args);
-        enableRawMode();
+        t.enableRawMode();
         initEditor();
 
         while (true){
@@ -75,9 +78,9 @@ public class Viewer {
     }
 
     private static void initEditor() {
-        LibC.Winsize windowSize = getWindowSize();
-        columns = windowSize.ws_col;
-        rows = windowSize.ws_row - 1;
+        Terminal.WindowSize windowSize = t.getWindowSize();
+        columns = windowSize.columns();
+        rows = windowSize.rows() - 1;
     }
 
     private static void refreshScreen() {
@@ -187,7 +190,7 @@ public class Viewer {
     private static void exit() {
         System.out.print("\033[2J");
         System.out.print("\033[H");
-        LibC.INSTANCE.tcsetattr(LibC.SYSTEM_OUT_FD, LibC.TCSAFLUSH, originalAttributes);
+        t.disableRawMode();
         System.exit(0);
     }
 
@@ -217,8 +220,27 @@ public class Viewer {
             }
     }
 
+}
 
-    private static void enableRawMode() {
+
+
+interface Terminal {
+    void enableRawMode();
+
+    void disableRawMode();
+
+    WindowSize getWindowSize();
+
+    record WindowSize(int rows, int columns) {}
+}
+
+
+class UnixTerminal implements Terminal {
+
+    private static LibC.Termios originalAttributes;
+
+    @Override
+    public void enableRawMode() {
         LibC.Termios termios = new LibC.Termios();
         int rc = LibC.INSTANCE.tcgetattr(LibC.SYSTEM_OUT_FD, termios);
 
@@ -240,7 +262,13 @@ public class Viewer {
     }
 
 
-    private static LibC.Winsize getWindowSize() {
+    @Override
+    public void disableRawMode() {
+        LibC.INSTANCE.tcsetattr(LibC.SYSTEM_OUT_FD, LibC.TCSAFLUSH, originalAttributes);
+    }
+
+    @Override
+    public WindowSize getWindowSize() {
         final LibC.Winsize winsize = new LibC.Winsize();
 
         final int rc = LibC.INSTANCE.ioctl(LibC.SYSTEM_OUT_FD, LibC.INSTANCE.TIOCGWINSZ, winsize);
@@ -250,66 +278,401 @@ public class Viewer {
             System.exit(1);
         }
 
-        return winsize;
+        return new WindowSize(winsize.ws_row, winsize.ws_col);
+    }
+
+
+    interface LibC extends Library {
+
+        int SYSTEM_OUT_FD = 0;
+        int ISIG = 1, ICANON = 2, ECHO = 10, TCSAFLUSH = 2,
+                IXON = 2000, ICRNL = 400, IEXTEN = 100000, OPOST = 1, VMIN = 6, VTIME = 5, TIOCGWINSZ = 0x5413;
+
+        // we're loading the C standard library for POSIX systems
+        LibC INSTANCE = Native.load("c", LibC.class);
+
+        @Structure.FieldOrder(value = {"ws_row", "ws_col", "ws_xpixel", "ws_ypixel"})
+        class Winsize extends Structure {
+            public short ws_row, ws_col, ws_xpixel, ws_ypixel;
+        }
+
+
+
+        @Structure.FieldOrder(value = {"c_iflag", "c_oflag", "c_cflag", "c_lflag", "c_cc"})
+        class Termios extends Structure {
+            public int c_iflag, c_oflag, c_cflag, c_lflag;
+
+            public byte[] c_cc = new byte[19];
+
+            public Termios() {
+            }
+
+            public static Termios of(Termios t) {
+                Termios copy = new Termios();
+                copy.c_iflag = t.c_iflag;
+                copy.c_oflag = t.c_oflag;
+                copy.c_cflag = t.c_cflag;
+                copy.c_lflag = t.c_lflag;
+                copy.c_cc = t.c_cc.clone();
+                return copy;
+            }
+
+            @Override
+            public String toString() {
+                return "Termios{" +
+                        "c_iflag=" + c_iflag +
+                        ", c_oflag=" + c_oflag +
+                        ", c_cflag=" + c_cflag +
+                        ", c_lflag=" + c_lflag +
+                        ", c_cc=" + Arrays.toString(c_cc) +
+                        '}';
+            }
+        }
+
+
+        int tcgetattr(int fd, Termios termios);
+
+        int tcsetattr(int fd, int optional_actions,
+                      Termios termios);
+
+        int ioctl(int fd, int opt, Winsize winsize);
+
+    }
+}
+
+
+class WindowsTerminal implements Terminal {
+
+    private IntByReference inMode;
+    private IntByReference outMode;
+
+    @Override
+    public void enableRawMode() {
+        Pointer inHandle = Kernel32.INSTANCE.GetStdHandle(Kernel32.STD_INPUT_HANDLE);
+
+        inMode = new IntByReference();
+        Kernel32.INSTANCE.GetConsoleMode(inHandle, inMode);
+
+        int inMode;
+        inMode = this.inMode.getValue() & ~(
+                Kernel32.ENABLE_ECHO_INPUT
+                        | Kernel32.ENABLE_LINE_INPUT
+                        | Kernel32.ENABLE_MOUSE_INPUT
+                        | Kernel32.ENABLE_WINDOW_INPUT
+                        | Kernel32.ENABLE_PROCESSED_INPUT
+        );
+
+        inMode |= Kernel32.ENABLE_VIRTUAL_TERMINAL_INPUT;
+
+
+        Kernel32.INSTANCE.SetConsoleMode(inHandle, inMode);
+
+        Pointer outHandle = Kernel32.INSTANCE.GetStdHandle(Kernel32.STD_OUTPUT_HANDLE);
+        outMode = new IntByReference();
+        Kernel32.INSTANCE.GetConsoleMode(outHandle, outMode);
+
+        int outMode = this.outMode.getValue();
+        outMode |= Kernel32.ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        outMode |= Kernel32.ENABLE_PROCESSED_OUTPUT;
+        Kernel32.INSTANCE.SetConsoleMode(outHandle, outMode);
+
+    }
+
+
+    @Override
+    public void disableRawMode() {
+        Pointer inHandle = Kernel32.INSTANCE.GetStdHandle(Kernel32.STD_INPUT_HANDLE);
+        Kernel32.INSTANCE.SetConsoleMode(inHandle, inMode.getValue());
+
+        Pointer outHandle = Kernel32.INSTANCE.GetStdHandle(Kernel32.STD_OUTPUT_HANDLE);
+        Kernel32.INSTANCE.SetConsoleMode(outHandle, outMode.getValue());
+    }
+
+
+    @Override
+    public WindowSize getWindowSize() {
+        final Kernel32.CONSOLE_SCREEN_BUFFER_INFO info = new Kernel32.CONSOLE_SCREEN_BUFFER_INFO();
+        final Kernel32 instance = Kernel32.INSTANCE;
+        final Pointer handle = Kernel32.INSTANCE.GetStdHandle(Kernel32.STD_OUTPUT_HANDLE);
+        instance.GetConsoleScreenBufferInfo(handle, info);
+        return new WindowSize(info.windowHeight(), info.windowWidth());
+    }
+    interface Kernel32 extends StdCallLibrary {
+
+        Kernel32 INSTANCE = Native.load("kernel32", Kernel32.class);
+
+        /**
+         * The CryptUIDlgSelectCertificateFromStore function displays a dialog box
+         * that allows the selection of a certificate from a specified store.
+         *
+         * @param hCertStore Handle of the certificate store to be searched.
+         * @param hwnd Handle of the window for the display. If NULL,
+         * defaults to the desktop window.
+         * @param pwszTitle String used as the title of the dialog box. If
+         * NULL, the default title, "Select Certificate,"
+         * is used.
+         * @param pwszDisplayString Text statement in the selection dialog box. If
+         * NULL, the default phrase, "Select a certificate
+         * you want to use," is used.
+         * @param dwDontUseColumn Flags that can be combined to exclude columns of
+         * the display.
+         * @param dwFlags Currently not used and should be set to 0.
+         * @param pvReserved Reserved for future use.
+         * @return Returns a pointer to the selected certificate context. If no
+         * certificate was selected, NULL is returned. When you have
+         * finished using the certificate, free the certificate context by
+         * calling the CertFreeCertificateContext function.
+         */
+        public static final int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004, ENABLE_PROCESSED_OUTPUT = 0x0001;
+
+        int ENABLE_LINE_INPUT = 0x0002;
+        int ENABLE_PROCESSED_INPUT = 0x0001;
+        int ENABLE_ECHO_INPUT = 0x0004;
+        int ENABLE_MOUSE_INPUT = 0x0010;
+        int ENABLE_WINDOW_INPUT = 0x0008;
+        int ENABLE_QUICK_EDIT_MODE = 0x0040;
+        int ENABLE_INSERT_MODE = 0x0020;
+
+        int ENABLE_EXTENDED_FLAGS = 0x0080;
+
+        int ENABLE_VIRTUAL_TERMINAL_INPUT = 0x0200;
+
+
+        int STD_OUTPUT_HANDLE = -11;
+        int STD_INPUT_HANDLE = -10;
+        int DISABLE_NEWLINE_AUTO_RETURN = 0x0008;
+
+        // BOOL WINAPI GetConsoleScreenBufferInfo(
+        // _In_   HANDLE hConsoleOutput,
+        // _Out_  PCONSOLE_SCREEN_BUFFER_INFO lpConsoleScreenBufferInfo);
+        void GetConsoleScreenBufferInfo(
+                Pointer in_hConsoleOutput,
+                CONSOLE_SCREEN_BUFFER_INFO out_lpConsoleScreenBufferInfo)
+                throws LastErrorException;
+
+        void GetConsoleMode(
+                Pointer in_hConsoleOutput,
+                IntByReference out_lpMode)
+                throws LastErrorException;
+
+        void SetConsoleMode(
+                Pointer in_hConsoleOutput,
+                int in_dwMode) throws LastErrorException;
+
+        Pointer GetStdHandle(int nStdHandle);
+
+        // typedef struct _CONSOLE_SCREEN_BUFFER_INFO {
+        //   COORD      dwSize;
+        //   COORD      dwCursorPosition;
+        //   WORD       wAttributes;
+        //   SMALL_RECT srWindow;
+        //   COORD      dwMaximumWindowSize;
+        // } CONSOLE_SCREEN_BUFFER_INFO;
+        class CONSOLE_SCREEN_BUFFER_INFO extends Structure {
+
+
+            public COORD dwSize;
+            public COORD dwCursorPosition;
+            public short wAttributes;
+            public SMALL_RECT srWindow;
+            public COORD dwMaximumWindowSize;
+
+            private static String[] fieldOrder = {"dwSize", "dwCursorPosition", "wAttributes", "srWindow", "dwMaximumWindowSize"};
+
+            @Override
+            protected java.util.List<String> getFieldOrder() {
+                return java.util.Arrays.asList(fieldOrder);
+            }
+
+            public int windowWidth() {
+                return this.srWindow.width() + 1;
+            }
+
+            public int windowHeight() {
+                return this.srWindow.height() + 1;
+            }
+        }
+
+        // typedef struct _COORD {
+        //    SHORT X;
+        //    SHORT Y;
+        //  } COORD, *PCOORD;
+        class COORD extends Structure implements Structure.ByValue {
+            public COORD() {
+            }
+
+            public COORD(short X, short Y) {
+                this.X = X;
+                this.Y = Y;
+            }
+
+            public short X;
+            public short Y;
+
+            private static String[] fieldOrder = {"X", "Y"};
+
+            @Override
+            protected java.util.List<String> getFieldOrder() {
+                return java.util.Arrays.asList(fieldOrder);
+            }
+        }
+
+        // typedef struct _SMALL_RECT {
+        //    SHORT Left;
+        //    SHORT Top;
+        //    SHORT Right;
+        //    SHORT Bottom;
+        //  } SMALL_RECT;
+        class SMALL_RECT extends Structure {
+            public SMALL_RECT() {
+            }
+
+            public SMALL_RECT(SMALL_RECT org) {
+                this(org.Top, org.Left, org.Bottom, org.Right);
+            }
+
+            public SMALL_RECT(short Top, short Left, short Bottom, short Right) {
+                this.Top = Top;
+                this.Left = Left;
+                this.Bottom = Bottom;
+                this.Right = Right;
+            }
+
+            public short Left;
+            public short Top;
+            public short Right;
+            public short Bottom;
+
+            private static String[] fieldOrder = {"Left", "Top", "Right", "Bottom"};
+
+            @Override
+            protected java.util.List<String> getFieldOrder() {
+                return java.util.Arrays.asList(fieldOrder);
+            }
+
+            public short width() {
+                return (short) (this.Right - this.Left);
+            }
+
+            public short height() {
+                return (short) (this.Bottom - this.Top);
+            }
+
+        }
+
+
     }
 
 }
 
-interface LibC extends Library {
 
-    int SYSTEM_OUT_FD = 0;
-    int ISIG = 1, ICANON = 2, ECHO = 10, TCSAFLUSH = 2,
-            IXON = 2000, ICRNL = 400, IEXTEN = 100000, OPOST = 1, VMIN = 6, VTIME = 5, TIOCGWINSZ = 0x5413;
 
-    // we're loading the C standard library for POSIX systems
-    LibC INSTANCE = Native.load("c", LibC.class);
+class MacOSTerminal implements Terminal {
 
-    @Structure.FieldOrder(value = {"ws_row", "ws_col", "ws_xpixel", "ws_ypixel"})
-    class Winsize extends Structure {
-        public short ws_row, ws_col, ws_xpixel, ws_ypixel;
+    private static LibC.Termios originalAttributes;
+
+    @Override
+    public void enableRawMode() {
+        LibC.Termios termios = new LibC.Termios();
+        int rc = LibC.INSTANCE.tcgetattr(LibC.SYSTEM_OUT_FD, termios);
+
+        if (rc != 0) {
+            System.err.println("There was a problem calling tcgetattr");
+            System.exit(rc);
+        }
+
+        originalAttributes = LibC.Termios.of(termios);
+
+        termios.c_lflag &= ~(LibC.ECHO | LibC.ICANON | LibC.IEXTEN | LibC.ISIG);
+        termios.c_iflag &= ~(LibC.IXON | LibC.ICRNL);
+        termios.c_oflag &= ~(LibC.OPOST);
+
+       /* termios.c_cc[LibC.VMIN] = 0;
+        termios.c_cc[LibC.VTIME] = 1;*/
+
+        LibC.INSTANCE.tcsetattr(LibC.SYSTEM_OUT_FD, LibC.TCSAFLUSH, termios);
     }
 
 
+    @Override
+    public void disableRawMode() {
+        LibC.INSTANCE.tcsetattr(LibC.SYSTEM_OUT_FD, LibC.TCSAFLUSH, originalAttributes);
+    }
 
-    @Structure.FieldOrder(value = {"c_iflag", "c_oflag", "c_cflag", "c_lflag", "c_cc"})
-    class Termios extends Structure {
-        public int c_iflag, c_oflag, c_cflag, c_lflag;
+    @Override
+    public WindowSize getWindowSize() {
+        final LibC.Winsize winsize = new LibC.Winsize();
 
-        public byte[] c_cc = new byte[19];
+        final int rc = LibC.INSTANCE.ioctl(LibC.SYSTEM_OUT_FD, LibC.INSTANCE.TIOCGWINSZ, winsize);
 
-        public Termios() {
+        if (rc != 0) {
+            System.err.println("ioctl failed with return code[={}]" + rc);
+            System.exit(1);
         }
 
-        public static Termios of(Termios t) {
-            Termios copy = new Termios();
-            copy.c_iflag = t.c_iflag;
-            copy.c_oflag = t.c_oflag;
-            copy.c_cflag = t.c_cflag;
-            copy.c_lflag = t.c_lflag;
-            copy.c_cc = t.c_cc.clone();
-            return copy;
-        }
-
-        @Override
-        public String toString() {
-            return "Termios{" +
-                    "c_iflag=" + c_iflag +
-                    ", c_oflag=" + c_oflag +
-                    ", c_cflag=" + c_cflag +
-                    ", c_lflag=" + c_lflag +
-                    ", c_cc=" + Arrays.toString(c_cc) +
-                    '}';
-        }
+        return new WindowSize(winsize.ws_row, winsize.ws_col);
     }
 
 
-    int tcgetattr(int fd, Termios termios);
+    interface LibC extends Library {
 
-    int tcsetattr(int fd, int optional_actions,
-                     Termios termios);
+        int SYSTEM_OUT_FD = 0;
+        int ISIG = 1, ICANON = 2, ECHO = 10, TCSAFLUSH = 2,
+                IXON = 2000, ICRNL = 400, IEXTEN = 100000, OPOST = 1, VMIN = 6, VTIME = 5, TIOCGWINSZ = 0x40087468;
 
-    int ioctl(int fd, int opt, Winsize winsize);
+        // we're loading the C standard library for POSIX systems
+        LibC INSTANCE = Native.load("c", LibC.class);
 
+        @Structure.FieldOrder(value = {"ws_row", "ws_col", "ws_xpixel", "ws_ypixel"})
+        class Winsize extends Structure {
+            public short ws_row, ws_col, ws_xpixel, ws_ypixel;
+        }
+
+
+
+        @Structure.FieldOrder(value = {"c_iflag", "c_oflag", "c_cflag", "c_lflag", "c_cc"})
+        class Termios extends Structure {
+            public long c_iflag, c_oflag, c_cflag, c_lflag;
+
+            public byte[] c_cc = new byte[19];
+
+            public Termios() {
+            }
+
+            public static Termios of(Termios t) {
+                Termios copy = new Termios();
+                copy.c_iflag = t.c_iflag;
+                copy.c_oflag = t.c_oflag;
+                copy.c_cflag = t.c_cflag;
+                copy.c_lflag = t.c_lflag;
+                copy.c_cc = t.c_cc.clone();
+                return copy;
+            }
+
+            @Override
+            public String toString() {
+                return "Termios{" +
+                        "c_iflag=" + c_iflag +
+                        ", c_oflag=" + c_oflag +
+                        ", c_cflag=" + c_cflag +
+                        ", c_lflag=" + c_lflag +
+                        ", c_cc=" + Arrays.toString(c_cc) +
+                        '}';
+            }
+        }
+
+
+        int tcgetattr(int fd, Termios termios);
+
+        int tcsetattr(int fd, int optional_actions,
+                      Termios termios);
+
+        int ioctl(int fd, int opt, Winsize winsize);
+
+    }
 }
+
+
+
 
     /**/
